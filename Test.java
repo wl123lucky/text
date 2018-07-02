564c08629b2c62a80331838316c92bfcd27ed593
Lvsi-China <15239928381@163.com>
2018-07-02 10:37:31 +0800

    修改了Test

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("hello");
+		System.out.println("喝酒断片");
+		System.out.println("你好");
+		System.out.println("ooo");
+
+	}
+}
